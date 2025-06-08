2dabd2b1fa6db88b12fe2c5d601fbc36ea44678e
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-07 20:23:04 -0400

    Create variables.tf

---
+++ syslogForwarder/networkingConfig/variables.tf
@@ -0,0 +1,28 @@
+# version 0.1 #
+
+variable "resource_tags" {
+  description = "define tags for your resources"
+  default = {
+    enviornment = "SentinelResources"
+  }
+}
+
+variable "subscription_id" {
+  default     = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  description = "define the Azure subscription's ID"
+}
+
+variable "resource_group_location" {
+  default     = "East Us"
+  description = "define the Azure region resources will reside in"
+}
+
+variable "resource_group_name" {
+  default     = "Sentinel-RG"
+  description = "Name the resource group your Sentinel resources will reside in"
+}
+
+variable "approved_ip" {
+  default     = "XXX.XXX.XXX.XXX/32"
+  description = "Specify approved IP. Utilize source_address_prefixes if you require more than 1 IP"
+}
