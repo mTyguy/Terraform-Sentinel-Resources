1b8a6ce5cc49f97d50b821d0ea5eba5176e152a5
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-08 20:57:26 -0400

    Update output.tf

--- syslogForwarder/networkingConfig/output.tf
+++ syslogForwarder/networkingConfig/output.tf
@@ -1,4 +1,12 @@
-# version 0.1 #
+# version 0.2 #
+
+output "syslogForwarder_rg_name" {
+  value = azurerm_resource_group.syslogForwarder.name
+}
+
+output "syslogForwarder_rg_location" {
+  value = azurerm_resource_group.syslogForwarder.location
+}
 
 output "syslogForwarder_subnet_id" {
   value = azurerm_subnet.syslogForwarder-subnet.id
