24f3a78397f2474a424341938dd3be7049f9ba0d
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 12:44:35 -0400

    Update output.tf

--- deploySentinel/output.tf
+++ deploySentinel/output.tf
@@ -1,5 +1,13 @@
-# version 0.1 #
+# version 0.2 #
 
 output "sentinel_onboarding_workspace_id" {
   value = azurerm_sentinel_log_analytics_workspace_onboarding.terra-sentinel-onboarding.workspace_id
+}
+
+output "sentinel_rg_name" {
+  value = azurerm_resource_group.terra-sentinel.name
+}
+
+output "sentinel_rg_location" {
+  value = azurerm_resource_group.terra-sentinel.location
 }
