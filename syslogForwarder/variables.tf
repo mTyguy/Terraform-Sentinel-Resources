74d86f6e7cced5434c27a67afe7eaca024ce4be7
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-08 21:02:00 -0400

    Update variables.tf
    simplify variables

--- syslogForwarder/variables.tf
+++ syslogForwarder/variables.tf
@@ -11,13 +11,3 @@ variable "subscription_id" {
   default     = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
   description = "define the Azure subscription's ID"
 }
-
-variable "resource_group_location" {
-  default     = "East Us"
-  description = "define the Azure region resources will reside in"
-}
-
-variable "resource_group_name" {
-  default     = "Sentinel-RG"
-  description = "Name the resource group your Sentinel resources will reside in"
-}
