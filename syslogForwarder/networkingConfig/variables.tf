da858dba2c35302813176390ec5aab22d266e47e
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 21:43:10 -0400

    Update variables.tf
    streamline resource group referencing

--- syslogForwarder/networkingConfig/variables.tf
+++ syslogForwarder/networkingConfig/variables.tf
@@ -1,4 +1,4 @@
-# version 0.1 #
+# version 0.2 #
 
 variable "resource_tags" {
   description = "define tags for your resources"
@@ -10,16 +10,6 @@ variable "resource_tags" {
 variable "subscription_id" {
   default     = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
   description = "define the Azure subscription's ID"
-}
-
-variable "resource_group_location" {
-  default     = "East Us"
-  description = "define the Azure region resources will reside in"
-}
-
-variable "resource_group_name" {
-  default     = "Sentinel-RG"
-  description = "Name the resource group your Sentinel resources will reside in"
 }
 
 variable "approved_ip" {
