ed93108f179d732203f8c7456f4b1e5c4253a5e8
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 21:41:40 -0400

    Update output.tf
    streamline resource group referencing

--- syslogForwarder/networkingConfig/output.tf
+++ syslogForwarder/networkingConfig/output.tf
@@ -1,12 +1,4 @@
-# version 0.2 #
-
-output "syslogForwarder_rg_name" {
-  value = azurerm_resource_group.syslogForwarder.name
-}
-
-output "syslogForwarder_rg_location" {
-  value = azurerm_resource_group.syslogForwarder.location
-}
+# version 0.3 #
 
 output "syslogForwarder_subnet_id" {
   value = azurerm_subnet.syslogForwarder-subnet.id
