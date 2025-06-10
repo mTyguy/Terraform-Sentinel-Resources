372d86ab13b7fe10fcbbb54ec74599f44e4f9ed5
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 21:54:57 -0400

    Create variables.tf

---
+++ syslogForwarder/dataCollection/variables.tf
@@ -0,0 +1,18 @@
+# version 0.1 #
+
+variable "resource_tags" {
+  description = "define tags for your resources"
+  default = {
+    enviornment = "SentinelResources"
+  }
+}
+
+variable "subscription_id" {
+  default     = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+  description = "define the Azure subscription's ID"
+}
+
+variable "resource_location" {
+  default     = "eastus"
+  description = "define the Azure subscription's ID"
+}
