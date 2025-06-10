43b39a0afbe74fe618277e030a04508c9c9c9e12
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 21:47:58 -0400

    Update main.tf
    streamlined resource group referencing, added system identity and virtual machine extension

--- syslogForwarder/main.tf
+++ syslogForwarder/main.tf
@@ -1,10 +1,18 @@
-# version 0.2 #
+# version 0.3 #
 
 #deploy public IP, NIC, & Linux VM
 
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
+}
+
+data "terraform_remote_state" "rg_config" {
+  backend = "local"
+
+  config = {
+    path = "../enchancedDeployment/deploySentinel/terraform.tfstate"
+  }
 }
 
 data "terraform_remote_state" "network_config" {
@@ -21,8 +29,8 @@ data "terraform_remote_state" "network_config" {
 
 resource "azurerm_network_interface" "syslogForwarder-NIC" {
   name                = "syslogForwarder-NIC"
-  location            = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_location
-  resource_group_name = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_name
+  location            = data.terraform_remote_state.rg_config.outputs.sentinel_rg_location
+  resource_group_name = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
 
   ip_configuration {
     name                          = "internal"
@@ -38,23 +46,22 @@ resource "azurerm_network_interface" "syslogForwarder-NIC" {
 
 resource "azurerm_public_ip" "syslogForwarder-pubIP" {
   name                = "syslogForwarder-pubIP"
-  resource_group_name = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_name
-  location            = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_location
+  resource_group_name = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
+  location            = data.terraform_remote_state.rg_config.outputs.sentinel_rg_location
   allocation_method   = "Static"
 
   tags = var.resource_tags
 }
 
-
-#create linux VM, disk, ssh keys
+#create linux VM, disk, work with ssh keys, and run AMA script
 
 resource "azurerm_linux_virtual_machine" "syslogForwarder-VM" {
   admin_username                  = "admin"
   computer_name                   = "syslogForwarder"
   disable_password_authentication = true
   name                            = "syslogForwarder-VM"
-  resource_group_name             = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_name
-  location                        = data.terraform_remote_state.network_config.outputs.syslogForwarder_rg_location
+  resource_group_name             = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
+  location                        = data.terraform_remote_state.rg_config.outputs.sentinel_rg_location
   size                            = "Standard_B1ms" /* 1vCPU 2gbRam - change per needs*/
   network_interface_ids = [
     azurerm_network_interface.syslogForwarder-NIC.id
@@ -62,7 +69,7 @@ resource "azurerm_linux_virtual_machine" "syslogForwarder-VM" {
 
   admin_ssh_key {
     username   = "admin"
-    public_key = file("publickey_id_rsa.pub")
+    public_key = file("linuxForwarder_id_rsa.pub")
   }
 
   os_disk {
@@ -77,17 +84,36 @@ resource "azurerm_linux_virtual_machine" "syslogForwarder-VM" {
     version   = "latest"
   }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
       user        = "admin"
-      private_key = file("/home/admin/id_rsa")
+      private_key = file("/home/admin/.ssh/id_rsa")
       host        = azurerm_public_ip.syslogForwarder-pubIP.ip_address
     }
     inline = [
       "sudo wget -O Forwarder_AMA_installer.py https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/Syslog/Forwarder_AMA_installer.py&&sudo python3 Forwarder_AMA_installer.py"
     ]
   }
+
+  tags = var.resource_tags
+
+}
+
+#create vm extension required for solution
+
+resource "azurerm_virtual_machine_extension" "syslogForwarderAzureMonitorLinuxAgent" {
+  name                       = "syslogForwarderAzureMonitorLinuxAgent"
+  virtual_machine_id         = azurerm_linux_virtual_machine.syslogForwarder-VM.id
+  publisher                  = "Microsoft.Azure.Monitor"
+  type                       = "AzureMonitorLinuxAgent"
+  type_handler_version       = "1.6"
+  auto_upgrade_minor_version = true
+  automatic_upgrade_enabled  = true
 
   tags = var.resource_tags
 
