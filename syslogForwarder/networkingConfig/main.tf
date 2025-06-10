e577bc26276afd206c3332cd5511a18815cf23a3
Tyler <119826083+mTyguy@users.noreply.github.com>
2025-06-09 21:41:06 -0400

    Update main.tf
    streamlined resource group referencing

--- syslogForwarder/networkingConfig/main.tf
+++ syslogForwarder/networkingConfig/main.tf
@@ -1,4 +1,4 @@
-# version 0.1 #
+# version 0.2 #
 
 #deploy Vnet, subnet, NSG, & NSG rules for linux syslogForwarder machine
 
@@ -7,13 +7,12 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-#create resource group
+data "terraform_remote_state" "rg_config" {
+  backend = "local"
 
-resource "azurerm_resource_group" "Sentinel-RG" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-
-  tags = var.resource_tags
+  config = {
+    path = "../../enchancedDeployment/deploySentinel/terraform.tfstate"
+  }
 }
 
 ###
@@ -22,28 +21,29 @@ resource "azurerm_resource_group" "Sentinel-RG" {
 
 resource "azurerm_virtual_network" "syslogForwarder-Net" {
   name                = "syslogForwarder-network"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = data.terraform_remote_state.rg_config.outputs.sentinel_rg_location
+  resource_group_name = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
   address_space       = ["192.168.1.0/24"]
-  dns_servers         = ["1.1.1.2"] /* DNS servers of your choice*/
+  dns_servers         = ["1.1.1.2"]
 
   tags = var.resource_tags
 }
 
 resource "azurerm_subnet" "syslogForwarder-subnet" {
   name                 = "syslogForwarder-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
   virtual_network_name = azurerm_virtual_network.syslogForwarder-Net.name
   address_prefixes     = ["192.168.1.0/24"]
 }
 
 ###
+
 #create NSG, rules, and association with subnet
 
 resource "azurerm_network_security_group" "syslogForwarder-Nsg" {
   name                = "syslogForwarder-NSG"
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
+  resource_group_name = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
+  location            = data.terraform_remote_state.rg_config.outputs.sentinel_rg_location
 }
 
 resource "azurerm_network_security_rule" "syslogForwarder-NgsRule-AllowSSHin" {
@@ -56,7 +56,7 @@ resource "azurerm_network_security_rule" "syslogForwarder-NgsRule-AllowSSHin" {
   destination_port_range      = "22"
   source_address_prefix       = var.approved_ip
   destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
+  resource_group_name         = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
   network_security_group_name = azurerm_network_security_group.syslogForwarder-Nsg.name
 }
 
@@ -70,7 +70,7 @@ resource "azurerm_network_security_rule" "syslogForwarder-NsgRules-Allow514in" {
   destination_port_range      = "514"
   source_address_prefix       = var.approved_ip
   destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
+  resource_group_name         = data.terraform_remote_state.rg_config.outputs.sentinel_rg_name
   network_security_group_name = azurerm_network_security_group.syslogForwarder-Nsg.name
 }
 
